f33873e6adfe335253d1a86242cb942758708b47
kyungboshim <kyungbo.shim@neowiz.com>
2025-04-22 16:45:04 +0900

    test

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
     products: [
         .library(
             name: "AdiscopeMediaMaxAdapterDT",
-            type: .dynamic,
             targets: ["AdiscopeMediaMaxDTTarget"]),
     ],
     dependencies: [
