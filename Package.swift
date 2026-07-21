2a83781d5fdaf6889476785b23bea7c207dccb0b
kyungboshim <kyungbo.shim@neowiz.com>
2026-07-21 09:22:50 +0900

    upm test

--- Package.swift
+++ Package.swift
@@ -12,33 +12,27 @@ let package = Package(
             targets: ["AdiscopeMediaMaxDTTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.5.1"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.2.0"),
     ],
     targets: [
         .target(
             name: "AdiscopeMediaMaxDTTarget",
             dependencies: [
-                .target(name: "REVUPMediaMaxAdapterMoloco"),
-                .target(name: "REVUPMediaMaxAdapterMolocoMediationAdapter"),
-                .target(name: "MolocoSDK"),
-                .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
+                .target(name: "AdiscopeMediaMaxAdapterAdMob"),
+                .target(name: "AdiscopeMediaMaxAdapterAdMobMediationAdapter"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
             ],
             path: "Sources"
         ),
         .binaryTarget(
-            name: "REVUPMediaMaxAdapterMoloco",
-            url: "https://github.com/revup-tnk/REVUP-iOS-SDK/releases/download/1.0.0/REVUPMediaMaxAdapterMoloco.zip",
-            checksum: "e5af035c8aab68c00b75a6483d81567b99e9fb185ca039546e4d4864eb40c7f0"
+            name: "AdiscopeMediaMaxAdapterAdMob",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.4.0/AdiscopeMediaMaxAdapterAdMob.zip",
+            checksum: "42ba2e1825242b4b32141c8e2a1004f5f5906055924d7d357fa07021dbeef854"
         ),
         .binaryTarget(
-            name: "REVUPMediaMaxAdapterMolocoMediationAdapter",
-            url: "https://github.com/revup-tnk/REVUP-iOS-SDK/releases/download/1.0.0/AppLovinMediationMolocoAdapter.xcframework.zip",
-            checksum: "02f625f5ec197b00e4639c68d149b62968a9ffd50d88b981c076f9a5b11b42cd"
-        ),
-        .binaryTarget(
-            name: "MolocoSDK",
-            url: "https://github.com/revup-tnk/REVUP-iOS-SDK/releases/download/1.0.0/MolocoSDK.xcframework.zip",
-            checksum: "0e0deaec5993d71e13a67328ed4fd8ecc74f9bdb89527dbabc18126c3bdfb80b"
+            name: "AdiscopeMediaMaxAdapterAdMobMediationAdapter",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.4.0/AppLovinMediationGoogleAdapter.xcframework.zip",
+            checksum: "d3ccbcecc0fb7f7e4a82737979801e1b913742f1f2fc3c6c73c7f3e2ace1a7d4"
         ),
     ]
 )
